892fa2052bb913ca20463cb08db0b96ac12b15ef
amin367 <aganji367@gmail.com>
2021-07-10 21:01:55 +0430

    change

--- js/replace.js
+++ js/replace.js
@@ -1,18 +1,27 @@
-
+// exercise 00: (replace developer and developer words at the same time)
 let sentence = "I am web developer and I am senior";
 
-statement.replace("developer","student").replace("Senior","Junior")
+// method 1 (advance shape)
 
-console.log(replace);
+const mapObj = {
+  developer: "student",
+  senior: "junior",
+};
+
+phrase = phrase.replace(
+  /\b(?:developer|senior)\b/gi,
+  (matched) => mapObj[matched]
+);
+
+// method 2 (beginner friendly)
+
+sentence.replace("developer", "student").replace("senior", "junior");
 
 // exercise 01: (replace JavaScript and interpreting words at the same time)
 let phrase = "JavaScript is an interpreting programming language.";
 
-// method 2 (beginner friendly)
-
-phrase.replace("JavaScript", "C#").replace("interpreting", "compiling");
-
 // method 1 (advance shape)
+
 const mapObj = {
   JavaScript: "C#",
   interpreting: "compiling",
@@ -22,3 +31,7 @@ phrase = phrase.replace(
   /\b(?:JavaScript|interpreting)\b/gi,
   (matched) => mapObj[matched]
 );
+
+// method 2 (beginner friendly)
+
+phrase.replace("JavaScript", "C#").replace("interpreting", "compiling");
