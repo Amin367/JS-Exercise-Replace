865f27ce044b05baade01129d84d187f8e7fc2f2
amin367 <aganji367@gmail.com>
2021-07-10 20:47:47 +0430

    initial commits

--- js/replace.js
+++ js/replace.js
@@ -1,5 +1,24 @@
+
 let sentence = "I am web developer and I am senior";
 
 statement.replace("developer","student").replace("Senior","Junior")
 
 console.log(replace);
+
+// exercise 01: (replace JavaScript and interpreting words at the same time)
+let phrase = "JavaScript is an interpreting programming language.";
+
+// method 2 (beginner friendly)
+
+phrase.replace("JavaScript", "C#").replace("interpreting", "compiling");
+
+// method 1 (advance shape)
+const mapObj = {
+  JavaScript: "C#",
+  interpreting: "compiling",
+};
+
+phrase = phrase.replace(
+  /\b(?:JavaScript|interpreting)\b/gi,
+  (matched) => mapObj[matched]
+);
